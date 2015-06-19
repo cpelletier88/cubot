b4bb23ec4cfa3001c85d76d9531450264b83d581
Chris Pelletier <cpelletier@barracuda.com>
2015-06-19 12:05:40 -0700

    Remove boa function

--- scripts/cudasign.js
+++ scripts/cudasign.js
@@ -1,5 +1,5 @@
 module.exports = function(robot) {
     robot.respond(/get me a basic token/i, function(res) {
-    	res.reply(btoa(process.env.CS_CLIENT_ID + ':' + process.env.CS_CLIENT_SECRET));
+    	res.reply(process.env.CS_CLIENT_ID + ':' + process.env.CS_CLIENT_SECRET);
     });
 };
