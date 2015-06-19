0742a33ecd7df8c8dfaad65397a704fa6c6a875c
Chris Pelletier <cpelletier@barracuda.com>
2015-06-19 13:05:57 -0700

    Get token by environment

--- scripts/cudasign.js
+++ scripts/cudasign.js
@@ -1,5 +1,15 @@
 module.exports = function(robot) {
-    robot.respond(/get me a basic token/i, function(res) {
-    	res.reply(new Buffer(process.env.CS_CLIENT_ID + ':' + process.env.CS_CLIENT_SECRET).toString('base64'));
+
+	function getEncodedToken(env) {
+		if(env === 'eval' || !process.env.CS_CLIENT_ID) {
+			return 'MGZjY2RiYzczNTgxY2EwZjliZjhjMzc5ZTZhOTY4MTM6MzcxOWExMjRiY2ZjMDNjNTM0ZDRmNWMwNWI1YTE5NmI=';
+		} else {
+			return (new Buffer(process.env.CS_CLIENT_ID + ':' + process.env.CS_CLIENT_SECRET).toString('base64'));
+		}
+	}
+
+    robot.respond(/get me a basic token for (.*)/i, function(res) {
+    	var env = res.match[1].trim();
+    	res.reply(getEncodedToken(env));
     });
 };
