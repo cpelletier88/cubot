c89719509b4d8bf9016e27a201e0aa7bbd4a3d2f
Chris Pelletier <cpelletier@barracuda.com>
2015-06-19 12:59:20 -0700

    Base 64 encode the string

--- scripts/cudasign.js
+++ scripts/cudasign.js
@@ -1,5 +1,5 @@
 module.exports = function(robot) {
     robot.respond(/get me a basic token/i, function(res) {
-    	res.reply(process.env.CS_CLIENT_ID + ':' + process.env.CS_CLIENT_SECRET);
+    	res.reply(new Buffer(process.env.CS_CLIENT_ID + ':' + process.env.CS_CLIENT_SECRET).toString('base64'));
     });
 };
