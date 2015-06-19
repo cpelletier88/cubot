c7603f43f14b1984683cdeb6bbe7bb9ad954994f
Chris Pelletier <cpelletier@barracuda.com>
2015-06-19 12:52:10 -0700

    Please don't die cubit

--- scripts/cudasign.js
+++ scripts/cudasign.js
@@ -1,5 +1,5 @@
 module.exports = function(robot) {
     robot.respond(/get me a basic token/i, function(res) {
-    	res.reply(new Buffer(process.env.CS_CLIENT_ID + ':' + process.env.CS_CLIENT_SECRET, 'base64').toString('utf8'));
+    	res.reply(process.env.CS_CLIENT_ID + ':' + process.env.CS_CLIENT_SECRET);
     });
 };
