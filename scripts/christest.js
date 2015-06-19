2e70afbcc93571cca4047822849a841e8651810d
Chris Pelletier <cpelletier@barracuda.com>
2015-06-19 11:04:55 -0700

    Messing around

--- scripts/christest.js
+++ scripts/christest.js
@@ -1,6 +1,6 @@
 module.exports = function(robot) {
     robot.hear(/.+/i, function(res) {
-    	res.send("Hello " + res.message.user.name);
+    	res.send(res);
     	// if(res.message.user.name === 'Shell') {
     	// 	res.reply('shut up shell');
     	// }
