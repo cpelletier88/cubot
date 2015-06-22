0c5d34a5098b33d2b224a76f5755bfaf084b2810
Phi Dong <pdong@barracuda.com>
2015-06-22 09:29:20 -0700

    Add aabillity to ask cubot what is for lunch on another day.

--- scripts/test.js
+++ scripts/test.js
@@ -9,9 +9,12 @@ module.exports = function(robot) {
 
     });
 
-    robot.hear(/what is for lunch/i, function(msg){
+    robot.hear(/what is for lunch (.*)/i, function(msg){
+
+    	var passedDay = msg.match[1];
 
     	var moment = require('moment');
+    	var _ = require('lodash');
 
 		var lunchChoices = [
 			'Greek',
@@ -30,11 +33,52 @@ module.exports = function(robot) {
 			'Nothing'
 		];
 
+		var dayNames = [
+			'sunday',
+			'monday',
+			'tuesday',
+			'wednesday',
+			'thursday',
+			'friday',
+			'saturday'
+		];
+
 		var now = moment().subtract(7, 'hours');
+
+
+		if (passedDay.toLowerCase() === 'tomorrow') {
+			now.add(1, 'day'); 		
+		}
+
+		if ( _.indexOf(dayNames, passedDay.toLowerCase()) !== -1) {
+
+			if(passedDay.toLowerCase() === 'sunday' || passedDay.toLowerCase() === 'saturday') {
+				msg.send('It\'s the weekend!  Eat whatever you want! (lol)');
+				return;
+			}
+			
+			var today = moment().day();
+			var passedDayValue = _.indexOf(dayNames, passedDay.toLowerCase());
+
+			if(today < passedDayValue) {
+				now.add(passedDayValue - today, 'day');
+			} else if (today > passedDayValue) {
+				var daysToOffset = 7 - today + passedDayValue;
+
+				now.add(daysToOffset, 'day');
+			}
+		}
+
 		var daysFromStartPoint = Math.abs(moment('June 8, 2015').diff(now, 'days'));
 		daysFromStartPoint = daysFromStartPoint % lunchChoices.length;
 
-        msg.send('Today lunch is ' + lunchChoices[daysFromStartPoint]);
+        if(!passedDay) {
+        	msg.send('Today lunch is ' + lunchChoices[daysFromStartPoint]);
+        } else if (passedDay.toLowerCase() === 'tomorrow') {
+        	msg.send(passedDay + ' lunch will be ' + lunchChoices[daysFromStartPoint]);
+        } else {
+        	msg.send('Lunch will be ' + lunchChoices[daysFromStartPoint] + ' on ' + passedDay);
+        }
 
     });
 
