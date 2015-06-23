c0902d86669f2d6c7cb5c40d45f2e5ac0aa62bd4
Phi Dong <pdong@barracuda.com>
2015-06-23 12:37:30 -0700

    Oops

--- scripts/hipchat.js
+++ scripts/hipchat.js
@@ -1,27 +1,27 @@
-var querystring = require('querystring');
+// var querystring = require('querystring');
 
-module.exports = function(robot) {
-// https://api.hipchat.com/v2/room/1610182/notification?auth_token=process.env.HIPCHAT_API_KEY
+// module.exports = function(robot) {
+// // https://api.hipchat.com/v2/room/1610182/notification?auth_token=process.env.HIPCHAT_API_KEY
 
-	robot.hear(/lunch is here/i, function(res){
+// 	robot.hear(/lunch is here/i, function(res){
 
-		res.send('lunch!');
+// 		res.send('lunch!');
 
-		var data = JSON.stringify({
-    		color: 'green',
-    		message: 'Lunch is here! (yey)',
-    		notify: 'false',
-    		message_format: 'text'
-    	});
+// 		var data = JSON.stringify({
+//     		color: 'green',
+//     		message: 'Lunch is here! (yey)',
+//     		notify: 'false',
+//     		message_format: 'text'
+//     	});
 
-		robot.http('https://api.hipchat.com/v2/room/1610182/notification?auth_token=' + process.env.HIPCHAT_API_KEY)
-			.header('Content-Type', 'application/json')
-			.post(data)(function(err, res, body) {
+// 		robot.http('https://api.hipchat.com/v2/room/1610182/notification?auth_token=' + process.env.HIPCHAT_API_KEY)
+// 			.header('Content-Type', 'application/json')
+// 			.post(data)(function(err, res, body) {
 
-			});
+// 			});
 
 
     	
-	});
+// 	});
 
-}
+// }
