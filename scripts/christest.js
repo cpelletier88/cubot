789b22770d825eb7d2c9d71c3c778e5f4469067f
Chris Pelletier <cpelletier@barracuda.com>
2015-06-24 17:03:47 -0700

    Cabot is no longer mean to Etan

--- scripts/christest.js
+++ scripts/christest.js
@@ -1,25 +1,6 @@
 var _ = require('lodash');
 
 module.exports = function(robot) {
-	var etanResponses = [
-		'Leave me alone <%= user %>! (trolletan)', 
-		'You are really starting to annoy me <%= user %>. (disapproval)',
-		'@Mullen can you tell <%= user %> to stop bothering me? (facepalm)',
-		'@phi can you tell <%= user %> to leave me alone? '
-	];
-
-    robot.respond(/.+/i, function(res) {
-    	if (res.message.user.name === 'Etan Karni') {
-    		var response = res.random(etanResponses);
-    		var compiled = _.template(response);
-    		res.send(compiled({'user': res.message.user.name}));
-    	}
-    });
-
-    robot.respond(/(you are|you're) mean/i, function(res) {
-    	res.send('Only to @EtanKarni');
-    });
-
     robot.hear(/\(tableflip\)/i, function(res) {
     	res.reply("Please Respect Tables   ┬─┬ノ(ಠ_ಠノ)");
     });
