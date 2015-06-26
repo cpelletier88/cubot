334f441996b13e8a1ca9faa887119876faee4e56
Phi Dong <pdong@barracuda.com>
2015-06-26 13:45:51 -0700

    change to post

--- scripts/deployView.js
+++ scripts/deployView.js
@@ -1,8 +1,8 @@
 module.exports = function(robot) {
 	return robot.router.get('/hubot/deploy', function(req, res) {
-		html = '<html><body><form action="/hubot/deployrequest">' + 
+		html = '<html><body><form action="/hubot/deployrequest" method="post">' + 
 				'<input type="text" name="snappier" placeholder="snappier branch"></input>' +
-				'<button type="submit" value="submit"></button>' +
+				'<button type="submit" value="submit">REQUEST!</button>' +
 				'</form></body></html>';
 
 		return res.send(html);
