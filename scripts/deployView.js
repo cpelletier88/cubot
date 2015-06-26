509b1b394b29303c7cf1a83139f97c7512c5bbc5
Phi Dong <pdong@barracuda.com>
2015-06-26 13:28:54 -0700

    test

--- scripts/deployView.js
+++ scripts/deployView.js
@@ -1,5 +1,5 @@
 module.exports = function(robot) {
 	return robot.router.post('/hubot/deploy', function(req, res) {
-		return '<html><body>wat</body></html>'
+		return '<html><body>wat</body></html>';
 	}
 }
