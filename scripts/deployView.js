40c8d6e76d941911eb227d08dcb0763e03d16194
Phi Dong <pdong@barracuda.com>
2015-06-26 13:37:57 -0700

    This is breaking cubot I think

--- scripts/deployView.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(robot) {
-	return robot.router.get('/hubot/deploy', function(req, res) {
-		var html =  '<html><body>wat</body></html>';
-		res.type('html');
-		return res.send(html);
-	}
-}
