05e2ddc07512e8cfb9243beb189f1efc3565f27f
Phi Dong <pdong@barracuda.com>
2015-06-26 14:07:43 -0700

    testing node templates..??

--- scripts/deployView.js
+++ scripts/deployView.js
@@ -1,9 +1,8 @@
 module.exports = function(robot) {
 	return robot.router.get('/hubot/deploy', function(req, res) {
-		html = '<html><body><form action="/hubot/requestdeploy" method="post">' + 
-				'<input type="text" name="snappier" placeholder="snappier branch"></input>' +
-				'<button type="submit" value="submit">REQUEST!</button>' +
-				'</form></body></html>';
+		var template = require('node-template');
+
+		var html = template.create('./views/deploy.tpl.html');
 
 		return res.send(html);
 
