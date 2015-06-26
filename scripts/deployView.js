6335fac4dbe67fef1fb66a206655252a4f5e9d7a
Phi Dong <pdong@barracuda.com>
2015-06-26 14:15:38 -0700

    ??

--- scripts/deployView.js
+++ scripts/deployView.js
@@ -1,7 +1,7 @@
 module.exports = function(robot) {
 	return robot.router.get('/hubot/deploy', function(req, res) {
 		var jade = require('jade');
-		var fn = jade.compile('./views/deploy.tpl.jade');
+		var fn = jade.compileFile('./views/deploy.tpl.jade');
 		var html = fn();
 
 		return res.send(html);
