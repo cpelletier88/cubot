98958b58d3d0fc794bb877b35ec7d0a4a74a232b
Phi Dong <pdong@barracuda.com>
2015-06-26 12:47:00 -0700

    test

--- scripts/deployRequest.js
+++ scripts/deployRequest.js
@@ -5,6 +5,22 @@ module.exports = function(robot) {
 		data = req.body.payload != null ? JSON.parse(req.body.payload) : req.body;
 		secret = data.secret;
 		robot.messageRoom('robots', 'testing');
-		return res.send('OK');
+
+
+
+		var data = JSON.stringify({
+    		color: 'green',
+    		message: 'Lunch is here! (yey)',
+    		notify: 'true',
+    		message_format: 'text'
+    	});
+		    	
+		robot.http('https://api.hipchat.com/v2/room/1610182/notification?auth_token=' + process.env.HIPCHAT_API_KEY)
+			.header('Content-Type', 'application/json')
+			.post(data)(function(err, res, body) {
+
+			});
 	});
 }
+
+curl -X POST -H "Content-Type: application/json" -d '{"secret":"C-TECH Astronomy"}' http://cudasign-cubot.herokuapp.com/hubot/requestdeploy
