da2dbbff59398a9cb8f6c29c7527b910a1d0f8e4
Phi Dong <pdong@barracuda.com>
2015-06-26 13:35:17 -0700

    wake up cubot

--- scripts/deployView.js
+++ scripts/deployView.js
@@ -1,6 +1,6 @@
 module.exports = function(robot) {
 	return robot.router.get('/hubot/deploy', function(req, res) {
-		html =  '<html><body>wat</body></html>';
+		var html =  '<html><body>wat</body></html>';
 		res.type('html');
 		return res.send(html);
 	}
