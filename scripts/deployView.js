f4ac9a54277d474179265af28ca33a3f55b99e6b
Phi Dong <pdong@barracuda.com>
2015-06-26 13:44:19 -0700

    ?

--- scripts/deployView.js
+++ scripts/deployView.js
@@ -1,11 +1,11 @@
 module.exports = function(robot) {
 	return robot.router.get('/hubot/deploy', function(req, res) {
-		html = '<html><body>wat</body></html>';
+		html = '<html><body><form action="/hubot/deployrequest">' + 
+				'<input type="text" name="snappier" placeholder="snappier branch"></input>' +
+				'<button type="submit" value="submit"></button>' +
+				'</form></body></html>';
 
 		return res.send(html);
 
 	});
 }
-
-// curl -X POST -H "Content-Type: application/json" -d '{"environment":"rc", "snapp":"master", "snstaticpages":"master", "tacostand":"master", "sql":"none"}' http://cudasign-cubot.herokuapp.com/hubot/requestdeploy
-// curl -X POST -H "Content-Type: application/json" -d '{"environment":"qa", "snappier":"master", "snwebapi":"develop"}' http://cudasign-cubot.herokuapp.com/hubot/requestdeploy
