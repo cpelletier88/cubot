aeae73a1f5d82baa6c0aa47a0b8ccbe00177a390
Phi Dong <pdong@barracuda.com>
2015-06-26 13:31:41 -0700

    cu cu cu cubot

--- scripts/deployView.js
+++ scripts/deployView.js
@@ -2,6 +2,6 @@ module.exports = function(robot) {
 	return robot.router.get('/hubot/deploy', function(req, res) {
 		html =  '<html><body>wat</body></html>';
 		res.type('html');
-		res.send(html);
+		return res.send(html);
 	}
 }
