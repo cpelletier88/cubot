b3e5a87a7237541a09ba8e5455f1f586047e06f3
Phi Dong <pdong@barracuda.com>
2015-06-26 13:46:48 -0700

    wrong endpoint name..

--- scripts/deployView.js
+++ scripts/deployView.js
@@ -1,6 +1,6 @@
 module.exports = function(robot) {
 	return robot.router.get('/hubot/deploy', function(req, res) {
-		html = '<html><body><form action="/hubot/deployrequest" method="post">' + 
+		html = '<html><body><form action="/hubot/requestdeploy" method="post">' + 
 				'<input type="text" name="snappier" placeholder="snappier branch"></input>' +
 				'<button type="submit" value="submit">REQUEST!</button>' +
 				'</form></body></html>';
