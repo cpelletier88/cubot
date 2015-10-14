71a58f2d49a25ad6afc36e7c8cbcca2d1f756543
Chris Pelletier <cpelletier@barracuda.com>
2015-10-14 11:50:21 -0700

    Testing

--- scripts/christest.js
+++ scripts/christest.js
@@ -36,7 +36,7 @@ module.exports = function(robot) {
     });
 
     robot.hear(/testing 123/i, function(msg) {
-        var roomId = msg.envelope;
+        var roomId = msg.envelope.user.reply_to.split('_')[0];
         msg.send('/code ' + JSON.stringify(roomId));
     });
 };
