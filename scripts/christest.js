1c90e9db3365750664490a734c8dd17f8ddbee13
Chris Pelletier <cpelletier@barracuda.com>
2015-10-14 09:14:50 -0700

    Hmm didn't work

--- scripts/christest.js
+++ scripts/christest.js
@@ -36,7 +36,7 @@ module.exports = function(robot) {
     });
 
     robot.hear(/testing 123/i, function(msg) {
-        var roomId = msg.envelope.user.reply_to.split('_')[0];
+        var roomId = msg.envelope.user.reply_to;
         msg.send('/code ' + roomId);
     });
 };
